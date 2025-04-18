b3faa32dfb82c80194cfa5d70843a1423b8881fd
kimsong <kimsongtuy18@gmail.com>
2025-04-18 17:48:03 +0700

    Profile Wokred

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Api\FavoriteApiController;
 use App\Http\Controllers\Api\CartApiController;
 use App\Http\Controllers\Api\PaymentApiController;
 use App\Http\Controllers\Api\OrderHistoryApiController;
+use App\Models\CityProvince;
 
 
 /*
@@ -65,11 +66,16 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/payments', [PaymentApiController::class, 'index']);
     Route::post('/payment', [PaymentApiController::class, 'store']);
     Route::get('/payments/{id}', [PaymentApiController::class,  'show']);
-    Route::get('/payments', [PaymentApiController::class, 'getUserPayments']);
 
 });    
 
 
 Route::middleware('auth:sanctum')->get('/order-history', [OrderHistoryApiController::class, 'orderHistory']);
+
+
+Route::get('/city-provinces', function () {
+    return response()->json(CityProvince::all());
+});
